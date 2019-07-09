b388e783ee63865a2f8b1e1cb36e8f37e74ec832
csamelian27 <csamelian@gmail.com>
2019-07-09 17:36:02 -0400

    added TS specific types

--- Using_Typescript/script.js
+++ Using_Typescript/script.js
@@ -26,3 +26,25 @@ var hobbies = ["Cooking", "Sports"];
 hobbies = [100];
 // error cause not arr
 // hobbies = 100;
+// new TS type: tuples - default arr: any[] type
+// let address = ["Superstreet", 99]
+// can override default by defining string and number types and they must be in that order
+var address = ["Superstreet", 99];
+// new TS type: emum - creates a new type we can use later on
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 2 OR (with assigned value above, it increments up from preceding value) 101
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor); // returns 1 - the index of the item;
+// any - should be an exception and avoided and explicitly define types instead
+var car = "BMW";
+// no error since defined as type any
+car = { brand: "BMW", series: 3 };
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
--- Using_Typescript/script.ts
+++ Using_Typescript/script.ts
@@ -5,15 +5,18 @@ let myName = "Max";
 // when we run the compiler, it will throw an error that we can't reassign a string to a number
 // myName = 28;
 
+
 // number - no difference between float and integer
 let myAge = 28;
 // will throw error
 // myAge = "Max";
 
+
 // boolean
 let hasHobbies = false;
 // will throw error
 // hasHobbies = 1;
+
 
 // assign types
 let myRealAge: number;
@@ -21,10 +24,12 @@ myRealAge = 27;
 // will throw error with explicit types
 // myRealAge = '27';
 
+
 // array
 // let hobbies = ["Cooking", "Sports"];
 // error because of string to number conversion
 // hobbies = [100]
+
 
 // can explicitly define as any and override ts type errors, still must be an arr
 let hobbies: any[] = ["Cooking", "Sports"];
@@ -32,3 +37,31 @@ let hobbies: any[] = ["Cooking", "Sports"];
 hobbies = [100];
 // error cause not arr
 // hobbies = 100;
+
+
+// new TS type: tuples - default arr: any[] type
+// let address = ["Superstreet", 99]
+// can override default by defining string and number types and they must be in that order
+let address: [string, number] = ["Superstreet", 99]
+
+
+// new TS type: emum - creates a new type we can use later on
+enum Color {
+  Gray, // 0
+  Green = 100, // 1 OR (with assigned value) 100
+  Blue // 2 OR (with assigned value above, it increments up from preceding value) 101
+}
+let myColor: Color = Color.Green;
+console.log(myColor); // returns 1 - the index of the item;
+
+// any - should be an exception and avoided and explicitly define types instead
+let car: any = "BMW";
+// no error since defined as type any
+car = { brand: "BMW", series: 3 };
+
+
+// functions
+function returnMyName() {
+  return myName;
+}
+console.log(returnMyName());
